69097088bf8a48e2c0f66f7161bb1f22422e9441
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-05-21 16:54:10 +0530

    adding exception class

---
+++ src/main/java/com/assessment/caculator/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package com.assessment.caculator;
+
+public class NegativeNumberException extends RuntimeException{
+
+    public NegativeNumberException(String message){
+
+        super(message);
+    }
+}
